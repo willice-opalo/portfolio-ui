2d082453ae2ab73dc75aef5ea432d80dfa2d9e33
Willice Opalo <williceopalo11@gmail.com>
2025-10-17 12:26:33 +0300

    fixed the home-page bug

--- app/components/FeaturedProjects.tsx
+++ app/components/FeaturedProjects.tsx
@@ -13,7 +13,7 @@ const FeaturedProjects = ({ projects, count = 4 }: FeaturedProjectsProps) => {
   return (
     <section>
       <h2 className='text-2xl font-bold mb-6 text-gray-200'>
-        🌟 Featured Projects
+        Featured Projects
       </h2>
 
       <div className='grid gap-6 sm:grid-cols-2'>
